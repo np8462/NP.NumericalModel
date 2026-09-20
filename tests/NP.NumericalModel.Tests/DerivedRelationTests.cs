7763e20aa2ec427251f623ed634c493ecc74a1ab
Navid Piri <np.test1984@gmail.com>
2026-09-20 23:45:44 +0330

    Test derivation from ratio compositions

--- tests/NP.NumericalModel.Tests/DerivedRelationTests.cs
+++ tests/NP.NumericalModel.Tests/DerivedRelationTests.cs
@@ -46,6 +46,44 @@ namespace NP.NumericalModel.Tests
         }
 
         [TestMethod]
+        public void CanDeriveComposedFortyFourOverSeven()
+        {
+            RatioComposition composition =
+                new RatioComposition(
+                    new Ratio(42, 7),
+                    new Ratio(2, 7));
+
+            RatioRelationDeriver deriver = new RatioRelationDeriver();
+            DerivedRelation relation = deriver.Derive(composition);
+
+            Assert.AreEqual(44L, relation.Numerator);
+            Assert.AreEqual(7L, relation.Denominator);
+            Assert.AreEqual(6L, relation.WholePart);
+            Assert.AreEqual(2L, relation.Remainder);
+            Assert.IsFalse(relation.IsExact);
+            Assert.AreEqual("6 + 2/7", relation.ToString());
+        }
+
+        [TestMethod]
+        public void CanDeriveExactComposedOne()
+        {
+            RatioComposition composition =
+                new RatioComposition(
+                    new Ratio(1, 2),
+                    new Ratio(1, 2));
+
+            RatioRelationDeriver deriver = new RatioRelationDeriver();
+            DerivedRelation relation = deriver.Derive(composition);
+
+            Assert.AreEqual(1L, relation.Numerator);
+            Assert.AreEqual(1L, relation.Denominator);
+            Assert.AreEqual(1L, relation.WholePart);
+            Assert.AreEqual(0L, relation.Remainder);
+            Assert.IsTrue(relation.IsExact);
+            Assert.AreEqual("1", relation.ToString());
+        }
+
+        [TestMethod]
         public void RejectsNullRatio()
         {
             RatioRelationDeriver deriver = new RatioRelationDeriver();
